a78f42b01d6d9ad9acc58452584ef847773bb729
caiopo <caiopoliveira@gmail.com>
2020-11-07 16:18:22 -0300

    Improve Song button

--- lib/songs.dart
+++ lib/songs.dart
@@ -1,6 +1,5 @@
 import 'dart:convert';
 
-import 'package:animated_size_and_fade/animated_size_and_fade.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
 import 'package:personal_site/utils.dart';
@@ -16,20 +15,14 @@ class _SongsButtonState extends State<SongsButton>
 
   @override
   Widget build(BuildContext context) {
-    return FloatingActionButton.extended(
-      icon: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 4),
-        child: Image.asset('assets/icons/music.png', width: 24, height: 24),
-      ),
-      label: AnimatedSizeAndFade(
-        vsync: this,
-        child: Text(
-          loading ? 'THINKING...' : 'SONG RECOMMENDATION',
+    return FloatingActionButton(
+      child: AnimatedSwitcher(
+        duration: const Duration(milliseconds: 300),
+        child: Image.asset(
+          loading ? 'assets/icons/hourglass.png' : 'assets/icons/music.png',
           key: Key('$loading'),
-          style: TextStyle(
-            fontWeight: FontWeight.w500,
-            fontSize: 20,
-          ),
+          width: 24,
+          height: 24,
         ),
       ),
       tooltip: 'Opens a random song that I\'m listening on repeat!',
